643941490c665b45ac3cf559ce5dcd2a2f407550
Mira Zubach <azzmiks@users.noreply.github.com>
2022-04-12 09:33:23 +0000

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,14 @@
-import org.jetbrains.dokka.gradle.DokkaTask
-
 plugins {
-    id("org.jetbrains.kotlin.jvm") version "1.6.10"
-    id("org.jetbrains.dokka") version "0.10.1"
+    id("org.jetbrains.kotlin.jvm") version "1.6.20"
+    id("org.jetbrains.dokka") version "1.6.10"
     id("org.jlleitschuh.gradle.ktlint") version "10.2.1"
     `maven-publish`
 }
 
 group = "com.personio"
 version = "1.0"
+
+apply(plugin = "org.jetbrains.dokka")
 
 repositories {
     mavenCentral()
@@ -28,14 +28,14 @@ val e2eTest =
 tasks.check { dependsOn(e2eTest) }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.6.10")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.6.20")
     api("javax.activation:activation:1.1.1")
-    api("com.datadoghq:datadog-api-client:1.3.0")
+    api("com.datadoghq:datadog-api-client:1.5.0")
     api("org.junit.jupiter:junit-jupiter-api:5.8.2")
     api("org.junit.jupiter:junit-jupiter-engine:5.8.2")
-    testImplementation("org.mockito:mockito-core:4.2.0")
-    testImplementation("org.mockito:mockito-inline:4.2.0")
-    testImplementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.1")
+    testImplementation("org.mockito:mockito-core:4.4.0")
+    testImplementation("org.mockito:mockito-inline:4.4.0")
+    testImplementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.2")
 }
 
 tasks {
@@ -47,14 +47,10 @@ tasks {
     }
 }
 
-val dokkaHtml by tasks.creating(DokkaTask::class) {
-    outputFormat = "html"
-    outputDirectory = "$buildDir/dokkaHtml"
-}
-
-val dokkaJar by tasks.creating(Jar::class) {
+val javadocJar by tasks.creating(Jar::class) {
+    dependsOn("dokkaJavadoc")
     archiveClassifier.set("javadoc")
-    from(dokkaHtml)
+    from(File(buildDir, "javadoc"))
 }
 
 val sourcesJar by tasks.creating(Jar::class) {
@@ -66,7 +62,7 @@ publishing {
     publications {
         register("maven", MavenPublication::class) {
             from(components["kotlin"])
-            artifact(dokkaJar)
+            artifact(javadocJar)
             artifact(sourcesJar)
             version =
                 if (project.hasProperty("release")) {
