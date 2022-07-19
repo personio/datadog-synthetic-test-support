13b326c4deeac81dc85cf1075c72a6ff1c715dd6
Daria Makovei <daria.makovei@personio.de>
2022-07-19 15:26:03 +0000

    [QA-1896] implement downloadedFileAssertion step

--- src/main/kotlin/com/personio/synthetics/step/assertion/SpecialAssertion.kt
+++ src/main/kotlin/com/personio/synthetics/step/assertion/SpecialAssertion.kt
@@ -1,11 +1,17 @@
 package com.personio.synthetics.step.assertion
 
-import com.datadog.api.v1.client.model.SyntheticsCheckType
 import com.datadog.api.v1.client.model.SyntheticsStep
 import com.datadog.api.v1.client.model.SyntheticsStepType
 import com.personio.synthetics.client.BrowserTest
 import com.personio.synthetics.model.assertion.AssertionParams
+import com.personio.synthetics.model.assertion.DownloadedFileAssertionParams
+import com.personio.synthetics.model.assertion.File
+import com.personio.synthetics.model.assertion.FileNameCheckType
+import com.personio.synthetics.model.assertion.FileSizeCheckType
+import com.personio.synthetics.model.assertion.NameCheck
+import com.personio.synthetics.model.assertion.SizeCheck
 import com.personio.synthetics.step.addStep
+import com.personio.synthetics.step.withParamType
 import org.intellij.lang.annotations.Language
 
 /**
@@ -33,15 +39,52 @@ fun BrowserTest.customJavascriptAssertion(
  * @param f Add all the parameters required for this test step
  * @return SyntheticsCommonAssertionSteps object with the downloaded file assertion step added
  */
-fun BrowserTest.downloadedFileAssertion(
-    stepName: String,
-    fileNameCheckType: SyntheticsCheckType,
-    expectedFileName: String,
-    fileSizeCheckType: SyntheticsCheckType,
-    expectedFileSize: Long,
-    f: (SyntheticsStep.() -> Unit)? = null
-) = addStep(stepName) {
-    type = SyntheticsStepType.ASSERT_FILE_DOWNLOAD
-    params = AssertionParams()
-    if (f != null) f()
+fun BrowserTest.downloadedFileAssertion(stepName: String, f: DownloadedFileAssertionStep.() -> Unit): DownloadedFileAssertionStep =
+    addStep(stepName, DownloadedFileAssertionStep()) {
+        type = SyntheticsStepType.ASSERT_FILE_DOWNLOAD
+        params = DownloadedFileAssertionParams(File())
+        f()
+    }
+
+/**
+ * Configure the Downloaded file assertion step for the synthetic browser test
+ */
+class DownloadedFileAssertionStep : SyntheticsStep() {
+    /**
+     * Sets the file name to be checked for the Downloaded file assertion step
+     * @param checkType The type of check to be done on the name of the downloaded file
+     * @param value Expected name for the Downloaded file
+     * @return DownloadedFileAssertionStep object with file.nameCheck param set
+     */
+    fun nameCheck(checkType: FileNameCheckType, value: String = "") = apply {
+        if (checkType !in listOf(FileNameCheckType.IS_EMPTY, FileNameCheckType.NOT_IS_EMPTY)) {
+            check(!value.isEmpty()) { "Expected value is a required parameter for the file name check in the step '${this.name}' when the passed check type is $checkType" }
+        }
+        params = withParamType<DownloadedFileAssertionParams> {
+            copy(file = file.copy(nameCheck = NameCheck(checkType.value.toString(), value)))
+        }
+    }
+
+    /**
+     * Sets the file size to be checked for the Downloaded file assertion step
+     * @param checkType Check type for the file size check of the Downloaded file assertion step
+     * @param value Expected file size in KB for the Downloaded file assertion step
+     * @return DownloadedFileAssertionStep object with file.sizeCheck param set
+     */
+    fun sizeCheck(checkType: FileSizeCheckType, value: Int) = apply {
+        params = withParamType<DownloadedFileAssertionParams> {
+            copy(file = file.copy(sizeCheck = SizeCheck(checkType.value.toString(), value)))
+        }
+    }
+
+    /**
+     * Sets the file MD5 value to be checked for the Downloaded file assertion step
+     * @param value Expected MD5 value for the Downloaded file assertion step
+     * @return DownloadedFileAssertionStep object with file.md5 param set
+     */
+    fun expectedMd5(value: String) = apply {
+        params = withParamType<DownloadedFileAssertionParams> {
+            copy(file = file.copy(md5 = value))
+        }
+    }
 }
