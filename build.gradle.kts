95cc231f50a083f444e9471101b2ee66d0b86745
serviceaccount-dependabot <product-dx+dependabot@personio.de>
2022-07-27 03:14:35 +0000

    Bump junit-jupiter-engine from 5.8.2 to 5.9.0 in /.

--- build.gradle.kts
+++ build.gradle.kts
@@ -44,7 +44,7 @@ dependencies {
     api("com.datadoghq:datadog-api-client:1.7.1")
     testRuntimeOnly("software.amazon.awssdk:sso:$awsSdkVersion")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
-    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.8.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.9.0")
     testImplementation("org.mockito:mockito-inline:4.6.1")
     testImplementation("org.mockito.kotlin:mockito-kotlin:4.0.0")
     testImplementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.3")
