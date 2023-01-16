ba6000dd7c133b79ca0d8e468cb5f437229b19f3
Emanuel Santana <22218826+emanuelsantan4@users.noreply.github.com>
2023-01-16 14:10:19 +0000

    Update writing as per guideline (#40)

--- src/main/kotlin/com/personio/synthetics/config/Message.kt
+++ src/main/kotlin/com/personio/synthetics/config/Message.kt
@@ -3,10 +3,10 @@ package com.personio.synthetics.config
 import com.personio.synthetics.client.BrowserTest
 
 /**
- * Configure the alert message to be sent when the test fails
+ * Configures the alert message to be sent when the test fails
  * @param failureMessage The message that needs to be sent to the configured alert medium upon test failure
- * @param alertMedium Specify one or more alert mediums. It can be slack channels, email addresses etc.
- * For slack channels, the channel name should be prefixed with "@slack-"
+ * @param alertMedium Specify one or more alert mediums. It can be Slack channels, email addresses etc.
+ * For Slack channels, the channel name should be prefixed with "@slack-"
  * For email address, the email should be prefixed with "@"
  * Example : Slack channel -> @slack-test_slack_channel
  * Email -> @firstName.lastName@domain.com
@@ -16,7 +16,7 @@ fun BrowserTest.alertMessage(failureMessage: String, vararg alertMedium: String)
 }
 
 /**
- * Configure the recovery message when the test recovers
+ * Configures the recovery message when the test recovers
  * @param recoveryMessage The message that needs to be sent upon recovery from a failure
  */
 fun BrowserTest.recoveryMessage(recoveryMessage: String) {
