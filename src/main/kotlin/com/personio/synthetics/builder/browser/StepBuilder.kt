3d64ca88dcce92625b3fb6c2cb64e406db34aece
Darmen Amanbayev <darmen.amanbayev@personio.de>
2023-11-10 14:27:50 +0100

    Remove `StepBuilder` class

--- src/main/kotlin/com/personio/synthetics/builder/browser/StepBuilder.kt
+++
@@ -1,7 +0,0 @@
-package com.personio.synthetics.builder.browser
-
-import com.datadog.api.client.v1.model.SyntheticsStep
-
-class StepBuilder(val name: String) {
-    fun build() = SyntheticsStep()
-}
