0cb1318dfb83aca29b50d4587c70a2859be59647
Mira Zubach <azzmiks@users.noreply.github.com>
2025-09-26 19:06:08 +0200

    Update WaitParams.kt

--- src/main/kotlin/com/personio/synthetics/model/actions/WaitParams.kt
+++ src/main/kotlin/com/personio/synthetics/model/actions/WaitParams.kt
@@ -2,7 +2,9 @@ package com.personio.synthetics.model.actions
 
 import com.personio.synthetics.model.Params
 
-internal data class WaitParams(val value: Int = 1) : Params() {
+internal data class WaitParams(
+    val value: Int = 1,
+) : Params() {
     init {
         require(value in 1..300) {
             "Waiting time must be no less than 1 and no bigger than 300 seconds."
