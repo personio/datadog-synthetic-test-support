b36bc5ed7f054e565f3d2dba0a57da7ea10a4435
Mira Zubach <azzmiks@users.noreply.github.com>
2025-09-26 19:07:37 +0200

    Update MonitorPriority.kt

--- src/main/kotlin/com/personio/synthetics/model/config/MonitorPriority.kt
+++ src/main/kotlin/com/personio/synthetics/model/config/MonitorPriority.kt
@@ -1,6 +1,8 @@
 package com.personio.synthetics.model.config
 
-enum class MonitorPriority(val priorityValue: Int) {
+enum class MonitorPriority(
+    val priorityValue: Int,
+) {
     P1_CRITICAL(1),
     P2_HIGH(2),
     P3_MEDIUM(3),
